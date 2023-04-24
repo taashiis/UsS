9320c392c48dbf8d272efdaa78b57c5c93138835
Anas Ahmad <79102789+01AnasAhmad@users.noreply.github.com>
2023-04-25 00:13:09 +0530

    added login

--- app.js
+++ app.js
@@ -7,9 +7,20 @@ const app = express();
 // //Password handler
 const bcrypt = require('bcrypt');
 
+// Using the CSRF token
+// const csrfProtection = csrf();
+// app.use(csrfProtection);
+
+
 // //Mongodb user model
 // const patientuser = require("./models/patientuser");
 const signupuser = require("./models/signupuser");
+
+// app.use((req, res, next) => {
+//   res.locals.csrfToken = req.csrfToken();
+//   res.locals.isLoggedIn = req.session.isLoggedIn;
+//   next();
+// })
 
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -37,6 +48,19 @@ app.get("/dashboard", function (req, res) {
 app.get("/verify", function (req, res) {
   res.render("verify");
 });
+// Jab yeh
+// app.get('/logout', function (req, res) {
+
+//   req.session.destroy(function (err) {
+//       if (err){
+//           console.log(err)
+//       }
+//       else{
+//           res.redirect('/')
+//       }
+//   })
+// })
+
 // // All post requests from forms
 
 app.post("/patientSignup",function(req,res){
@@ -86,9 +110,50 @@ app.post("/patientSignup",function(req,res){
 app.post("/login", function (req, res) {
   var loginData = req.body;
   // res.send(loginData);
-  let{email,pin}=loginData;
-  email = loginData.email;
-  pin = loginData.pin;
+  let{emaill,pinn}=loginData;
+  emaill = loginData.email;
+  pinn = loginData.pin;
+  check = 1;
+  if(emaill=="" || pinn==""){
+    res.json({
+        status:"FAILED",
+        message:"Empty Credentials Supplied"
+    })
+    check = 0;
+  }
+  if(check==1){
+    signupuser.find({emaill})
+    .then(data=>{
+      if(data){
+      // User exists
+      const hpin = "$2b$10$g/qEACj0G7DRAF0riiagCe.N1oeml05RJdVIhcmKY6JY4N/gxJRuq";
+      const hpinn = data[0].pin
+      bcrypt.compare(pinn, hpinn).then(result=>{
+      if(result){
+        console.log("Signin succesful")
+        check = 0;
+        // req.session.email = email;
+        // req.session.type = type;
+        otp = Math.floor(Math.random()*900000 + 100000)
+        mail.sendMail(emaill, otp)
+        // req.session.isLoggedIn = true;
+        res.redirect('/verify');
+      }
+      else{
+        res.json({
+        status:"FAILED",
+        message:"Invalid Password entered!"
+        })
+      }
+      }).catch(err=>{
+          res.json({
+          status:"FAILED",
+          message:"An error Occured while checking for existing user!"
+          })
+        })
+      }
+    })
+}
 });
 
 // app.post("/patientSignup", function (req, res) {
